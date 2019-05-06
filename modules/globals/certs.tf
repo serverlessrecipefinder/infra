47e8178827dd55718eed5bf3d2e2e99f678cb272
Jan AKerman <j.f.akerman@gmail.com>
2019-05-06 16:24:26 +0100

    Adds wildcard certificate
    ARN is stored as an 'output' in SSM.

---
+++ modules/globals/certs.tf
@@ -0,0 +1,23 @@
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "*.recipefinder.io"
+  validation_method = "DNS"
+  tags = "${var.tags}"
+}
+
+data "aws_route53_zone" "zone" {
+  name         = "recipefinder.io."
+  private_zone = false
+}
+
+resource "aws_route53_record" "cert_validation" {
+  name    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_name}"
+  type    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_type}"
+  zone_id = "${data.aws_route53_zone.zone.id}"
+  records = ["${aws_acm_certificate.cert.domain_validation_options.0.resource_record_value}"]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = "${aws_acm_certificate.cert.arn}"
+  validation_record_fqdns = ["${aws_route53_record.cert_validation.fqdn}"]
+}
--- modules/globals/outputs.tf
+++ modules/globals/outputs.tf
@@ -5,3 +5,12 @@ resource "aws_ssm_parameter" "cognito_user_pool_arn" {
   value = "${aws_cognito_user_pool.pool.arn}"
   overwrite = true
 }
+
+resource "aws_ssm_parameter" "certificate_arn" {
+  type  = "String"
+  description = "ARN of wildcard certificate for recipefinder.io"
+  name  = "/${var.prefix}/globals/certificate_arn"
+  value = "${aws_acm_certificate.cert.arn}"
+  overwrite = true
+}
+
